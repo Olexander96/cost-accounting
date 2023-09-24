988db4948682b849739b22837bcf07b92fb12bbd
Olexander96 <o.sheiko96@gmail.com>
2023-09-24 16:07:43 +0300

    added filter cost

--- src/components/Costs/Costs.js
+++ src/components/Costs/Costs.js
@@ -1,22 +1,35 @@
 import Card from "../UI/Card";
 import CostItem from "./CostItem";
-import './Costs.css';
+import CostsFilter from "./CostsFilter";
+
+import "./Costs.css";
+import React, { useState } from "react";
 
 const Costs = (props) => {
+  const costsArr = props.costs.map((item) => (
+    <CostItem
+      key={props.costs.indexOf(item)}
+      date={item.date}
+      description={item.description}
+      cost={item.amount}
+    />
+  ));
 
-    const costsArr = props.costs
-        .map(item => <CostItem 
-            key={props.costs.indexOf(item)} 
-            date={item.date} 
-            description={item.description} 
-            cost={item.amount}
-        />);
+  const [selectedYear, setSelectedYear] = useState("2022");
 
-    return (
-        <Card className="costs">
-            { costsArr }
-        </Card> 
-    );
-}
+  const setCurrentYearHandler = (year) => {
+    setSelectedYear(year);
+  };
+  console.log(selectedYear, "Cost component");
+
+  return (
+    <div>
+      <Card className="costs">
+        <CostsFilter onChangeYear={setCurrentYearHandler} year={selectedYear} />
+        {costsArr}
+      </Card>
+    </div>
+  );
+};
 
 export default Costs;
